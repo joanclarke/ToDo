7ef67d00cafe8d6874c277e04cc9122690acd621
Your Name <joanclarke100@gmail.com>
2020-09-29 00:27:56 -0500

    add delete icon on each list

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,55 @@
 
 
-            // create an empty array
-            // let myArr = ["A","B","C","D"];
-            let arr = [];
-            //get value for button and add event listener
-            let button = document.getElementById('button');
-            button.addEventListener('click', function() {
-              pushData();
-              clearInputField();
-            });
-            // button.addEventListener('click', pushData);
-            // button.addEventListener('click', clearInputField);
+ // create an empty array
+ // let myArr = ["A","B","C","D"];
+let arr = [];
+//get value for button and add event listener
+let button = document.getElementById('button');
+button.addEventListener('click', function() {
+  pushData();
+  clearInputField();
+});
+// span.addEventListener('click', function() {
+//   this.li.classList.add('remove');
+// });
+// button.addEventListener('click', pushData);
+// button.addEventListener('click', clearInputField);
 
-            function pushData()
-            {
-                // get value from the input text
-                let inputText = document.getElementById('inputText').value;
+function pushData() {
+    // get value from the input text
+    let inputText = document.getElementById('inputText').value;
                 
-                // append data to the array
-                arr.push(inputText);
+    // append data to the array
+    arr.push(inputText);
+    var text = "";
                 
-                var text = "";
-                
-                for(i = 0; i < arr.length; i++)
-                {
-                    // text = text + arr[i] + "<br/>";
-                    text = arr[i];
-                    
-                }
-     
-                if(text != "") {
-                  createList(text);
-                  
-                }
-                
-            }
+    for(i = 0; i < arr.length; i++) {
+      // text = text + arr[i] + "<br/>";
+      text = arr[i];
+    }
+              
+    if(text != "") {
+      createList(text);
+    }
+}
 
-            function createList(item) {
-              //create list 
-              let li = document.createElement("li");
-              // append list item to parent element (ul)
-              document.getElementById('list').appendChild(li);
-              //display data in list format
-              li.innerHTML = item;
+function createList(item) {
+    //create list 
+    let li = document.createElement("li");
+    // append list item to parent element (ul)
+    document.getElementById('list').appendChild(li);
+    //display data in list format
+    li.innerHTML = item + " <span><i id='delete' class='delete fa fa-trash'></i></span>";
+    li.setAttribute("class", "list-item");
+    
+    // <i class="fa fa-trash" aria-hidden="true"></i>
+    // console.log(arr);
+}
 
-              console.log(arr);
-            }
-
-            function clearInputField() {
-              //clear text from input field
-              document.getElementById('inputText').value = "";
-            }
+function clearInputField() {
+      //clear text from input field
+  document.getElementById('inputText').value = "";
+}
 
 
 
