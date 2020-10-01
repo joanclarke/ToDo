0c07f053cf420c59d0a2f6b68db234c8c0fe9bc6
Your Name <joanclarke100@gmail.com>
2020-09-30 23:47:53 -0500

    delete data when trash bin icon is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@ button.addEventListener('click', function() {
 // button.addEventListener('click', pushData);
 // button.addEventListener('click', clearInputField);
 
+//Push user input to screen
 function pushData() {
     // get value from the input text
     let inputText = document.getElementById('inputText').value;
@@ -33,24 +34,38 @@ function pushData() {
     }
 }
 
-function createList(item) {
     //create list 
+function createList(item) {
+
     let li = document.createElement("li");
     // append list item to parent element (ul)
     document.getElementById('list').appendChild(li);
+    let span = document.createElement("span");
+    // let i = document.createElement("i");
+    span.innerHTML = "<i id='delete' class='delete fa fa-trash'></i>";
     //display data in list format
-    li.innerHTML = item + " <span><i id='delete' class='delete fa fa-trash'></i></span>";
-    li.setAttribute("class", "list-item");
-    
+   
+    li.innerHTML = item;
+    li.appendChild(span);
+    li.setAttribute("id", "list-item");
+  //remove item when trash bin icon is clicked
+    span.addEventListener('click', function() {
+      li.remove();
+    });
     // <i class="fa fa-trash" aria-hidden="true"></i>
     // console.log(arr);
 }
 
+//clear text from input field
 function clearInputField() {
-      //clear text from input field
+
   document.getElementById('inputText').value = "";
 }
 
+
+// function deleteData() {
+//   li.remove();
+// }
 
 
 // https://stackoverflow.com/questions/33004177/add-user-input-to-list-on-button-click-in-javascript
