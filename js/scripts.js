e1c6edf9baf6d755799fdff6c2ed6601395b5139
Your Name <joanclarke100@gmail.com>
2020-10-04 17:22:53 -0500

    removed last commit - commit message was incorrect

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 
 
- // create an empty array
- // let myArr = ["A","B","C","D"];
-let arr = [];
-//get value for button and add event listener
 let button = document.getElementById('button');
+let input = document.getElementById('inputText');
+let completedList = document.getElementById("completed-list");
+
+
+let arr = [];
+
+//get value for button and add event listener
 button.addEventListener('click', function() {
   pushData();
   clearInputField();
@@ -15,139 +18,128 @@ button.addEventListener('click', function() {
 // button.addEventListener('click', pushData);
 // button.addEventListener('click', clearInputField);
 
-//Push user input to screen
+//display user input on screen
 function pushData() {
-    // get value from the input text
-    let inputText = document.getElementById('inputText').value;
+  // get input value
+  let inputText = input.value;
+  // append data to the array
+  arr.push(inputText);
+  var text = "";
                 
-    // append data to the array
-    arr.push(inputText);
-    var text = "";
-                
-    for(i = 0; i < arr.length; i++) {
-      // text = text + arr[i] + "<br/>";
-      text = arr[i];
-    }
+  for(i = 0; i < arr.length; i++) {
+    text = arr[i];
+  }
               
-    if(text != "") {
-      createList(text);
-    }
+  if(text != "") {
+    createList(text);
+  }
 }
 
-    //create list 
-function createList(item) {
-
+//create list 
+function createList(userInput) {
   // create completed list 
   let liCompleted = document.createElement("li");
-  let completedList = document.getElementById("completed-list");
   let trashSpan = document.createElement("span");
   let checkCircleCompleted = document.createElement("span");
   let iCompleted = document.createElement("i");
 
-
-  // let completedList = document.getElementById("completed-list");
   // create list item
-// let liCompleted = document.createElement("li");
-
-// let trashSpan = document.createElement("span");
-// let checkCircleCompleted = document.createElement("span");
-// let iCompleted = document.createElement("i");
-
-
-    // create list item
-    let li = document.createElement("li");
-       //append list item to parent element (ul)
-    document.getElementById('list').appendChild(li);
-    li.setAttribute("class", "list-item");
+  let li = document.createElement("li");
+  //append list item to parent element (ul)
+  document.getElementById('list').appendChild(li);
+  li.setAttribute("class", "list-item");
     
-    let checkCircle = document.createElement("span");
-    let i = document.createElement("i");
-    i.setAttribute("class", "far fa-circle");
-    // checkCircle.innerHTML =  "<i class='far fa-circle'></i>";
-    // circle.innerHTML = "<i class='far fa-check-circle'></i>";
-    let p = document.createElement("p");
-    // make data/content editable
-    p.setAttribute("contenteditable", "true");
-    let deleteTrash = document.createElement("span");
-    deleteTrash.innerHTML = "<i id='delete' class='delete fa fa-trash'></i>";
-    //display data in paragraph format
-    p.innerHTML = item;
+  // let checkCircle = document.createElement("span");
 
-    // add checklist, data/content and delete to list
-    checkCircle.appendChild(i);
-    li.prepend(checkCircle);
-    li.appendChild(p);
-    li.appendChild(deleteTrash);
-    // li.setAttribute("id", "list-item");
-    // p.setAttribute("display", "inherit");
-  //  console.log(li);
-  
-    // https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Editable_content#How_does_it_work
-  //remove item when trash bin icon is clicked
-  // document.querySelector('#delete').addEventListener('click', function() {
-  //   li.remove();
-  // });
- 
+
+  let i = document.createElement("i");
+  i.setAttribute("class", "far fa-circle");
+  // checkCircle.innerHTML =  "<i class='far fa-circle'></i>";
+  // circle.innerHTML = "<i class='far fa-check-circle'></i>";
+  let p = document.createElement("p");
+  // make data/content editable
+  p.setAttribute("contenteditable", "true");
+
+
+ let checkCircle = document.createElement("span");
+  // console.log(circle);
+  // let circle;
+  // let  = createRadioButton(circle, p);
+
+
+  let deleteTrash = document.createElement("span");
+  deleteTrash.innerHTML = "<i id='delete' class='delete fa fa-trash'></i>";
+
+  p.innerHTML = userInput;  //add user input in paragraph (p) element
+
+  // add checklist, data/content and trash functions to list
+  checkCircle.appendChild(i);
+  li.prepend(checkCircle);
+  li.appendChild(p);
+  li.appendChild(deleteTrash);
 
   checkCircle.addEventListener('click', function() {
-   p.classList.toggle("slash");
-  editData(i, p);
-  //  if(i.className == "far fa-circle") {
-  //   i.className = "far fa-check-circle";
-  //    //content cannot be edited
-  //   p.setAttribute("contenteditable", "false");
-  //  } else {
-  //   i.className = "far fa-circle";
-  //   //content can be edited
-  //   p.setAttribute("contenteditable", "true");
+    toggleCheck(p)
+    editData(i, p);
+    transferCompletedData(i, p, li, trashSpan, iCompleted, completedList, checkCircleCompleted, liCompleted);
+  })
 
-  //  }
-  transferList(i, p, li, trashSpan, iCompleted, completedList, checkCircleCompleted, liCompleted);
-  // //  transfer list over to completed when circle is checked
-  //  if(i.className == "far fa-check-circle") {
-    
-  //     trashSpan.innerHTML = "<i id='complete-trash' class='complete-trash fa fa-trash'></i>";
-  //     // let iCompleted = document.createElement("i");
-  //     iCompleted.setAttribute("class", "far fa-check-circle");
-  //   //  liCompleted.appendChild(trashSpan);
-  //     completedList.appendChild(liCompleted);
-  //   //  completedList.innerHTML = li.textContent;
-  //   // liCompleted.innerHTML = li.textContent;
-  //   p.innerHTML = li.innerText;
-  //   checkCircleCompleted.appendChild(iCompleted);
-  //   liCompleted.prepend(checkCircleCompleted);
-  //   liCompleted.appendChild(p);
-  //    liCompleted.appendChild(trashSpan);
-  //     // liCompleted.innerHTML = li.innerHTML;
-  //     li.innerHTML = "";
-  //     // li.remove();
-  //       //  console.log(li);
-  //  } 
-
-
-});
-
-  checkCircleCompleted.addEventListener('click', function() {
-    li.innerHTML = liCompleted.innerHTML;
-        liCompleted.innerHTML = "";
+  checkCircleCompleted.addEventListener('click', () => {
+    transferData(li, liCompleted);
   });
 
   // Delete content
-  deleteTrash.addEventListener('click', function() {
-        li.remove();
-    });
 
-    trashSpan.addEventListener('click', function() {
-      liCompleted.remove();
+  // deleteTrash.addEventListener('click', deleteData(li));
+  deleteTrash.addEventListener('click', function() {
+  //   li.remove();
+  deleteData(li);
   });
+
+  trashSpan.addEventListener('click', function() {
+    // liCompleted.remove();
+    deleteData(liCompleted);
+  });
+ 
 }
+
+
+function deleteData(x) {
+x.remove();
+// console.log(x);
+}
+
+
+function transferData(li, liCompleted) {
+  li.innerHTML = liCompleted.innerHTML;
+  liCompleted.innerHTML = "";
+}
+
+
+
+
+// function createRadioButton(circle, p, i) {
+
+//   circle.addEventListener('click', function() {
+//     p.classList.toggle("slash");
+//     editData(i, p);
+//     transferCompletedData(i, p, li, trashSpan, iCompleted, completedList, checkCircleCompleted, liCompleted);
+//   });
+// }
+
+function toggleCheck(p) {
+    p.classList.toggle("slash");
+//     editData(i, p);
+//     transferCompletedData(i, p, li, trashSpan, iCompleted, completedList, checkCircleCompleted, liCompleted);
+}
+
 
 //clear text from input field
 function clearInputField() {
   document.getElementById('inputText').value = "";
 }
  
-// edit content only when circle does not have a tick inside of it
+// user can edit content only when circle does not have a tick inside of it
 function editData(i, p) {
   if(i.className == "far fa-circle") {
     i.className = "far fa-check-circle";
@@ -159,9 +151,13 @@ function editData(i, p) {
     p.setAttribute("contenteditable", "true");
   }
 }
-  
+
+  //create new list item 
+  function createNewListItem() {
+
+  }
 //  transfer list completed data/content over to completed-list container
-function transferList(i, p, li, trashSpan, iCompleted, completedList, checkCircleCompleted, liCompleted) {
+function transferCompletedData(i, p, li, trashSpan, iCompleted, completedList, checkCircleCompleted, liCompleted) {
   if(i.className == "far fa-check-circle") {
     trashSpan.innerHTML = "<i id='complete-trash' class='complete-trash fa fa-trash'></i>";
     iCompleted.setAttribute("class", "far fa-check-circle");
@@ -188,6 +184,7 @@ function transferList(i, p, li, trashSpan, iCompleted, completedList, checkCircl
 // App design link:
 // https://www.uplabs.com/posts/todo-list-mobile-app
 
-
+// https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Editable_content#How_does_it_work
+ 
 // MOVE CHILD
 // https://stackoverflow.com/questions/20910147/how-to-move-all-html-element-children-to-another-parent-using-javascript
